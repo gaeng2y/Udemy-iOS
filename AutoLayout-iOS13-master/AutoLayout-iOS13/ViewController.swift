d580b8868cad692057067910a61c1ee2f9537eba
g4eng <gaeng1107@gmail.com>
2022-04-12 21:31:59 +0900

    2022 04 12

--- AutoLayout-iOS13-master/AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13-master/AutoLayout-iOS13/ViewController.swift
@@ -9,15 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(imageLiteralResourceName: "GreenBackground"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
     
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(imageLiteralResourceName: "DiceeLogo"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
     
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    lazy var diceImageView1: UIImageView = {
+        let imageView = UIImageView(image: UIImage(imageLiteralResourceName: "DiceOne"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy var diceImageView2: UIImageView = {
+        let imageView = UIImageView(image: UIImage(imageLiteralResourceName: "DiceOne"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // background
+        self.view.addSubview(backgroundImageView)
+        [backgroundImageView.topAnchor.constraint(equalTo: self.view.topAnchor),
+         backgroundImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+         backgroundImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+         backgroundImageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)].forEach { contstraint in
+            contstraint.isActive = true
+        }
         
-        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        self.view.addSubview(logoImageView)
+        [logoImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150),
+         logoImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+         logoImageView.heightAnchor.constraint(equalToConstant: 70),
+         logoImageView.widthAnchor.constraint(equalToConstant: 200)].forEach { constraint in
+            constraint.isActive = true
+        }
         
-        diceImageView1.image = allDice.randomElement()
-        diceImageView2.image = allDice.randomElement()
+        self.view.addSubview(diceImageView1)
+        [diceImageView1.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300),
+         diceImageView1.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+         diceImageView1.heightAnchor.constraint(equalToConstant: 120),
+         diceImageView1.widthAnchor.constraint(equalToConstant: 120)].forEach { constraint in
+            constraint.isActive = true
+        }
+        
+        self.view.addSubview(diceImageView2)
+        [diceImageView2.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300),
+         diceImageView2.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
+         diceImageView2.heightAnchor.constraint(equalToConstant: 120),
+         diceImageView2.widthAnchor.constraint(equalToConstant: 120)].forEach { constraint in
+            constraint.isActive = true
+        }
     }
 }
